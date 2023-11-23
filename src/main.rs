068473c4360e969038f9e98b51bffaf62c81779d
Nathan Chin <nathanchin@nathanchin.com>
2023-11-23 09:58:06 -0500

    Encapsulate visualization in a struct

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ use std::io::Read;
 use clap::Parser;
 use macroquad::prelude::*;
 
-const HEIGHT: usize = 256;
-const WIDTH: usize = 256;
-const DEPTH: usize = 256;
-
 type BitMap = Vec<Vec<Vec<u32>>>;
 
 #[derive(Parser, Debug)]
@@ -15,14 +11,61 @@ struct Args {
     input_pathname: String,
 }
 
+struct Visualization {
+    buffer: BitMap,
+}
+
+impl Visualization {
+    const HEIGHT: usize = 256;
+    const WIDTH: usize = 256;
+    const DEPTH: usize = 256;
+
+    fn new_from_bytes(bytes: &[u8]) -> Self {
+        let mut buffer: BitMap = vec![vec![vec![0; Self::DEPTH]; Self::WIDTH]; Self::HEIGHT];
+
+        for pair in bytes.windows(3) {
+            let x = pair[0] as usize;
+            let y = pair[1] as usize;
+            let z = pair[2] as usize;
+
+            buffer[z][y][x] += 1;
+        }
+
+        Self { buffer }
+    }
+
+    fn draw(&self) {
+        for z in 0..Self::DEPTH {
+            for y in 0..Self::HEIGHT {
+                for x in 0..Self::WIDTH {
+                    if self.buffer[z][y][x] > 0 {
+                        let brightness = ((self.buffer[z][y][x] as f32).log(1.01) * 0.192)
+                            .clamp(0.0, 255.0) as u8;
+
+                        if brightness < 15 {
+                            continue;
+                        }
+
+                        draw_cube(
+                            vec3(x as f32 - 128., y as f32 - 128., z as f32 - 128.),
+                            Vec3::ONE,
+                            None,
+                            Color::from_rgba(255, 255, 255, brightness),
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[macroquad::main("Binvis")]
 async fn main() {
     let args = Args::parse();
     let contents = read_file_contents(&args.input_pathname)
         .expect(format!("Could not read file {}", &args.input_pathname).as_str());
 
-    let mut buffer: BitMap = vec![vec![vec![0; DEPTH]; WIDTH]; HEIGHT];
-    populate_buffer(&mut buffer, &contents);
+    let vis = Visualization::new_from_bytes(&contents);
 
     let mut position = vec3(275., 275., 275.);
 
@@ -38,43 +81,9 @@ async fn main() {
             ..Default::default()
         });
 
-        draw_buffer(&buffer);
+        vis.draw();
 
         next_frame().await;
-    }
-}
-
-fn draw_buffer(buffer: &BitMap) {
-    for z in 0..DEPTH {
-        for y in 0..HEIGHT {
-            for x in 0..WIDTH {
-                if buffer[z][y][x] > 0 {
-                    let brightness =
-                        ((buffer[z][y][x] as f32).log(1.01) * 0.192).clamp(0.0, 255.0) as u8;
-
-                    if brightness < 15 {
-                        continue;
-                    }
-
-                    draw_cube(
-                        vec3(x as f32 - 128., y as f32 - 128., z as f32 - 128.),
-                        Vec3::ONE,
-                        None,
-                        Color::from_rgba(255, 255, 255, brightness),
-                    );
-                }
-            }
-        }
-    }
-}
-
-fn populate_buffer(buffer: &mut BitMap, source: &[u8]) {
-    for pair in source.windows(3) {
-        let x = pair[0] as usize;
-        let y = pair[1] as usize;
-        let z = pair[2] as usize;
-
-        buffer[z][y][x] += 1;
     }
 }
 
