f6d8a82199eaffb172bde6a3aa1d5e8c6556a5a7
Nathan Chin <nathanchin@nathanchin.com>
2025-05-18 22:30:29 -0400

    Make brightness threshold configurable
    This makes rendering dense things faster if you specify a higher
    threshold, and also lets you fine-tune the results for both dense and
    sparse things.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use raylib::prelude::*;
 struct Args {
     /// The path of the file to visualize.
     input_pathname: String,
+
+    /// The minimum "brightness" of a location in the input file to qualify for
+    /// being drawn. A higher number will make the visualizion show fewer points
+    /// and thus be dimmer.
+    #[arg(short = 't', long, default_value_t = 15)]
+    draw_threshold: u8,
 }
 
 struct Visualization {
@@ -18,7 +24,7 @@ impl Visualization {
     const WIDTH: usize = 256;
     const DEPTH: usize = 256;
 
-    fn new_from_bytes(bytes: &[u8]) -> Self {
+    fn new_from_bytes(bytes: &[u8], draw_threshold: u8) -> Self {
         let mut buffer = vec![vec![vec![0; Self::DEPTH]; Self::WIDTH]; Self::HEIGHT];
 
         for pair in bytes.windows(3) {
@@ -32,12 +38,13 @@ impl Visualization {
         let mut points = vec![];
 
         // Only want to draw sufficiently "bright" points
+        #[allow(clippy::needless_range_loop)]
         for z in 0..Self::DEPTH {
             for y in 0..Self::HEIGHT {
                 for x in 0..Self::WIDTH {
                     let brightness =
                         ((buffer[z][y][x] as f32).log(1.01) * 0.192).clamp(0.0, 255.0) as u8;
-                    if brightness >= 15 {
+                    if brightness >= draw_threshold {
                         points.push((x as f32 - 128., y as f32 - 128., z as f32 - 128.));
                     }
                 }
@@ -59,7 +66,7 @@ fn main() {
     let contents = read_file_contents(&args.input_pathname)
         .unwrap_or_else(|_| panic!("Could not read file {}", &args.input_pathname));
 
-    let vis = Visualization::new_from_bytes(&contents);
+    let vis = Visualization::new_from_bytes(&contents, args.draw_threshold);
 
     let (mut rl, thread) = raylib::init().size(1920, 1080).build();
     let mut camera = Camera3D::perspective(
